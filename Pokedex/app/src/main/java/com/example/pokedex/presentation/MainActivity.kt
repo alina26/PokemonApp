f39bbd57537de6cbe5d3e25a4f02c4d065dc529f
alina26 <alina26158@gmail.com>
2021-05-30 20:28:26 +0300

    added up button and changed icon

--- Pokedex/app/src/main/java/com/example/pokedex/presentation/MainActivity.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/presentation/MainActivity.kt
@@ -2,15 +2,24 @@ package com.example.pokedex.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.pokedex.R
-import com.example.pokedex.presentation.details.PokemonDetailsFragment
-import com.example.pokedex.presentation.list.PokemonListFragment
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp()
     }
 }
